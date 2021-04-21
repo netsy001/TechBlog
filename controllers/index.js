2539e6b7a2ac413aee281f8ccea4981a221d8757
netsy001 <nettem.surendra@gmail.com>
2021-04-21 21:44:33 +0930

    user

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
-
+const userRoutes = require('./userRoutes');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/api/userRoutes', userRoutes);
 
 
 module.exports = router;
